6874e3dfcd72896df05482f3009ff6c8eab861f2
GetmevaDev <devteam@getmeva.com>
2021-04-14 12:53:39 +0500

    fix map

--- src/components/Home/SectionContacts/SectionContacts.js
+++ src/components/Home/SectionContacts/SectionContacts.js
@@ -14,6 +14,9 @@ export function SectionContacts(){
                   Pretitle
               }
           }
+          mapContact: strapiContacts {
+              Key_Map
+          }
       }
   `)
 
@@ -27,9 +30,8 @@ export function SectionContacts(){
         </div>
         <div className={classes.map}>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.540836208673!2d-73.94290068459388!3d40.750127979327715!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c258d5ebd00001%3A0x502036dda620abfc!2sHealthy%20Living%20Pharmacy%20LIC!5e0!3m2!1sru!2s!4v1615990462387!5m2!1sru!2s"
-            width="600" height="450" style={{ border:0 }} allowFullScreen="" loading="lazy"
-          />
+            src={data.mapContact.Key_Map}
+            width="600" height="450" style={{ border:0, }} allowFullScreen="" loading="lazy"/>
         </div>
       </div>
     </section> : null
