38e6d9b226c4b2e02d80b1c4df840c6785ed980f
GetmevaDev <devteam@getmeva.com>
2021-04-19 18:47:02 +0500

    add phone section

---
+++ src/components/Home/SectionForLinkOnMobilePhone/SectionForLinkOnMobilePhone.js
@@ -0,0 +1,61 @@
+import React from "react"
+import {graphql, useStaticQuery} from "gatsby"
+import Vector from "../../../images/Vector.svg"
+import GooglePlay from "../../../images/google-play-badge.svg"
+import AppStore from "../../../images/Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917.svg"
+import * as classes from "./sectionForLinkOnMobilePhone.module.scss"
+
+
+export  function SectionForLinkOnMobilePhone(){
+
+  const data = useStaticQuery(graphql`
+      {
+          strapiHomePage {
+              Section_Mobile_Phone {
+                  Title
+                  List {
+                      List_item
+                      id
+                  }
+                  Link_App_Store
+                  Link_Google_Play
+                  Image {
+                      alternativeText
+                      url
+                  }
+              }
+          }
+      }
+  `);
+
+  const {Title, List, Link_App_Store, Link_Google_Play, Image} = data.strapiHomePage.Section_Mobile_Phone;
+
+  return(
+    <section>
+      <div className={`container row ${classes.flexStyles}`}>
+        <div className="list-block">
+           <h2 className={classes.title}>{Title}</h2>
+          <ul className={classes.listContainer}>
+            {
+              List.map(item => (
+                <li className={classes.list} key={item.id}>
+                  <img src={Vector} alt="" />
+                  {item.List_item}
+                </li>
+              ))
+            }
+          </ul>
+          <a className={classes.linkApp} href={Link_Google_Play}>
+            <img src={GooglePlay} alt="" />
+          </a>
+          <a className={classes.linkApp} href={Link_App_Store}>
+            <img src={AppStore} alt="" />
+          </a>
+        </div>
+        <div className={classes.imageBlock}>
+          <img src={Image[0].url} alt={Image[0].alternativeText} />
+        </div>
+      </div>
+    </section>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import { AboutUs } from "../components/Home/AboutUs/AboutUs"
 import { SectionConsultation } from "../components/common/SectionConsultation/SectionConsultation"
 import { SectionContacts } from "../components/Home/SectionContacts/SectionContacts"
 import "../scss/main.scss"
+import { SectionForLinkOnMobilePhone } from "../components/Home/SectionForLinkOnMobilePhone/SectionForLinkOnMobilePhone"
 
 const IndexPage = ({data}) => (
   <Layout marginSocialMedia={50}>
@@ -18,6 +19,7 @@ const IndexPage = ({data}) => (
       images={data.strapiHomePage.Section_Header.Slide_Images}
     />
     <Card data={data.strapiHomePage.Our_Advantages} variant={false} />
+    <SectionForLinkOnMobilePhone />
     <AboutUs />
     <SectionConsultation />
       <SectionContacts />
