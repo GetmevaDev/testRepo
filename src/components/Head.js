9ff6b25f755186587473d8afe3646fee7efa6ae9
GetmevaDev <devteam@getmeva.com>
2021-04-26 17:03:45 +0500

    add seo

--- src/components/Head.js
+++ src/components/Head.js
@@ -5,6 +5,12 @@ import Helmet from "react-helmet"
 export default function Head(){
   return(
     <Helmet>
+      <meta property="og:title" content="Long Island City Pharmacy - Healthy Living Pharmacy LIC: 11101" />
+      <meta property="og:description" content="Our Long Island City Pharmacy is the neighborhood healer with decades of experience. We merged homeopathic, holistic medicine with traditional pharmaceuticals." />
+      <meta property="twitter:title" content="Long Island City Pharmacy - Healthy Living Pharmacy LIC: 11101" />
+      <meta property="twitter:description" content="Our Long Island City Pharmacy is the neighborhood healer with decades of experience. We merged homeopathic, holistic medicine with traditional pharmaceuticals." />
+
+
       <link rel="preconnect"
             href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
@@ -13,6 +19,7 @@ export default function Head(){
             href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
       <link rel="stylesheet" type="text/css"
             href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+
     </Helmet>
   )
 }
