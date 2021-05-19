c6471f4548404f8f256a5fd6e73ecb67f3b0a80c
GetmevaDev <devteam@getmeva.com>
2021-05-19 13:20:56 +0500

    add link

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,6 +6,7 @@ import { PaymentMethods } from "./Footer/PaymentMethods/PaymentMethods"
 import { Logo } from "./Header-components/Logo/Logo"
 import { SocialMedia } from "./Header-components/Social-Media/SocialMedia"
 import { LastText } from "./Footer/LastText/LastText"
+import LogoM from "../images/111.svg"
 import * as classes from "./footerStyles.module.scss"
 
 export function Footer(){
@@ -19,8 +20,19 @@ export function Footer(){
       </div>
       <div className={`container row ${classes.lastBlock}`}>
         <Logo />
-        <SocialMedia className={classes.footerIcon} />
         <LastText />
+        <SocialMedia className={classes.footerIcon} />
+        <div className={`row manufacturer`}>
+          <div className={`row adaptive-m`} style={{
+            alignItems: `center`,
+            marginLeft: 55,
+            marginTop: 23,
+          }}>
+            <img src={LogoM} alt="" />
+            <p>Web Design & Digital Marketing <br/>
+              by <a href="https://robertgerov.com">Robert Gerov Media</a></p>
+          </div>
+        </div>
       </div>
     </footer>
   )
