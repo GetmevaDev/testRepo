4c9ec3f854914cec179f7be950deddf5e8d518a0
GetmevaDev <devteam@getmeva.com>
2021-10-03 15:01:19 +0500

    fix button popup

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -53,7 +53,9 @@ export function PopUp(){
       <div style={{
         maxWidth: MaxWidthBlock ? `${MaxWidthBlock}px` : 439,
         transform: `scale(${cleanPopUp  ? 1 :  0 })`,
-        transition: '.5s'
+        transition: '.5s',
+        display: 'flex',
+        flexDirection: 'column'
       }} className={classes.cardWarning}>
         <button onMouseDown={removeCard} onMouseUp={removeCard} className={classes.buttonClose} type={`button`}>
           <img src={CloseWarning} alt="" />
